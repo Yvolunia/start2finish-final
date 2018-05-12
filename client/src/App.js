18207c6e73a51530f099e707a3ade9d19003da8c
jelizabethscott <jescottphoto@gmail.com>
2018-05-11 23:36:24 -0400

    update

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import Profiles from "./pages/KidProfile";
 import Kids from "./pages/KidHome";
 // import Signup from "./pages/SignUp";
 // import Tracker from "./pages/AdultTracker";
-// import Home from "./pages/HomeScreen";
+import Home from "./pages/HomeScreen";
 // import Nav from "./components/Nav";
 
 
@@ -15,15 +15,12 @@ const App = () => (
     <div>
       {/* <Nav /> */}
       <Switch>
-        <Route exact path="/" component={Profiles} />
+        <Route exact path="/" component={Home} />
         <Route exact path="/profile" component={Profiles} />
         <Route exact path="/missions/:id" component={Missions} />
         <Route exact path="/kid" component={Kids} />
-        {/* <Route exact path="/SignUp" component={SignUp} /> */}
+        <Route exact path="/kid/:id" component={Kids} />
         <Route exact path="/missions" component={Missions} />
-        {/* <Route exact path="/Tracker" component={Tracker} />
-        <Route exact path="/kid" component={Kid} /> */}
-            
       </Switch>
     </div>
   </Router>
--- client/src/pages/HomeScreen/home.js
+++ client/src/pages/HomeScreen/home.js
@@ -1,64 +1,23 @@
-import React, {Component } rom 'react';
-import Button from '../../components/Button';
-import Missions from '../../components/Missions';
-import Nav from '../../components/nav';
-import Scorebox from '../../components/Scorebox';
+import React, {Component } from 'react';
+import Jumbotron from "../../components/Jumbotron"; 
+import { Link } from "react-router-dom";
+import { Col, Row, Container } from "../../components/Grid";
 
-class HomeScreen extends Component{
-  state = {
-    username: [],
-    password: "",
-    first name: "",
-    last name: "",
-    email: "",
-    about yourself: "",
-  };
-
-  componentDidMount() {
-    this.loadHomeScreen();
-  }
-
-  loadHomeScreen = () => {
-    API. getHomescreen()
-    .then(res =>
-      this.setState({username: res.date, password: "", first name: "", last name: "", email: "", about yourself: ""})
-      )
-    .catch(err => console.log(err));
-  };
-
-  handleInputChange = event => {
-    const {name, value } = event.target;
-    this.setState({
-      [name]: value
-    });
-  };
-
-  handleFormSubmit = event => {
-    event.preventDefault();
-    if(this.state.password && this.state.first name){
-      API.saveHomeScreen({
-        password: this.state.password,
-        first name: this.state.first name,
-        last name: this.state.last name,
-        email: this.state.email,
-        about yourself: this.state.about yourself
-      })
-
-        .then(res => this.loadHomeScreen())
-        .catch(err => console.log(err));
-    }
-  };
-
-  render() {
-    return (  
-        <Container fluid>
-                    <row>
-                        <col size = sm-8 col-sm-offset-2 text>
-                            <h1>Login &amp; Register Forms<h1>
-                              <h3>
-                                Start-2-Finish is an goal oriented application that allows parents to be able to set weekly goals for there
-                                children. Allowing the children to be able to work towards a weekly goal, that results in rewards at the end of the week. If the goals are not met by the end of the week, the task will reset and the child will have another week to obtain the reward from the tasks. 
-                              <h3>
-      )
-  }
+class Home extends Component {
+render() {
+return (
+<Container fluid>
+<Row>
+<Col size="md-6 sm-12">
+<Jumbotron>
+  <img src="../../../../start2finishLOGO.png"> </img>
+</Jumbotron>
+</Col>
+</Row>
+</Container>
+);
 }
+}
+
+
+export default Home;
