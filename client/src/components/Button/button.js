41cd15aebc023ccb5ed394bc8781904fcdb3f906
jelizabethscott <jescottphoto@gmail.com>
2018-05-11 23:35:07 -0400

    update

--- client/src/components/Button/button.js
+++ client/src/components/Button/button.js
@@ -1,1 +1,11 @@
-button.js
+import React from "react";
+import "./Button.css";
+
+// The ...props means, spread all of the passed props onto this element
+// That way we don't have to define them all individually
+const Button = props => (
+  <button className="btn btn" {...props}>
+  </button>
+);
+
+export default Button;
