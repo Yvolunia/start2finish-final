af05bb94c8f156f1a76ebdb6b12a3dc10d70b7f4
jelizabethscott <jescottphoto@gmail.com>
2018-05-12 00:36:11 -0400

    update

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,11 +1,11 @@
 const router = require("express").Router();
 const userRoutes = require("./user.js");
 const profileRoutes = require('./profile');
-{/*const homeRoutes = require('./home');*/}
+const homeRoutes = require('./home');
 const missionRoutes = require('./missions');
 
 // Routes
-{/*router.use("/home", homeRoutes);*/}
+router.use("/home", homeRoutes);
 router.use("/user", userRoutes);
 router.use('/profile', profileRoutes);
 router.use('/missions', missionRoutes);
