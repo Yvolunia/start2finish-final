ec90511863232855eff65653718741c945d60927
jelizabethscott <jescottphoto@gmail.com>
2018-05-12 00:16:31 -0400

    update

--- client/src/pages/HomeScreen/home.js
+++ client/src/pages/HomeScreen/home.js
@@ -2,6 +2,7 @@ import React, {Component } from 'react';
 import Jumbotron from "../../components/Jumbotron"; 
 import { Link } from "react-router-dom";
 import { Col, Row, Container } from "../../components/Grid";
+import API from "../../utils/API";
 
 class Home extends Component {
 render() {
